1ae0199d36adcc76f545dee6055b1462445c6bcf
manutuli <97235754+manutuli@users.noreply.github.com>
2022-01-07 18:29:29 +0100

    sass file
    ajout de fichier sass

---
+++ cvOnline/controller/script.js
@@ -0,0 +1,8 @@
+// class Anim_cv
+// private function dom-element-listener()
+// private function change-element-in-html()
+
+// var element = change-element-in-html(()=>dom-element-listener())
+
+// public function on-click-drag-and-drop(this.element)
+// public function on-hover-position-on-top(this.element)
